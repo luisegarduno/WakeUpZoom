4bb108f5047fe26993f5ac10384b726442fecc82
luisegarduno <luis.e.garduno@gmail.com>
2021-01-25 21:19:07 -0600

    - Finally. Just need to clean up now

--- WakeUpZoom.ps1
+++ WakeUpZoom.ps1
@@ -23,10 +23,31 @@ while($course -eq 'y'){
   cls
   Write-Host $art -ForegroundColor "Blue"
 
-  $crs = Read-Host -Prompt 'Course name ---> '    # Course name
-  $url = Read-Host -Prompt '     > URL '          # Zoom URL
-  $clk = Read-Host -Prompt '     > Time '         # Course start time
-  $freq = Read-Host -Prompt '     > Frequency '   # Course frequency
+  $crs = Read-Host -Prompt 'Course name ---> '                # Course name
+  $url = Read-Host -Prompt '     > URL '                      # Zoom URL
+  $clk = Read-Host -Prompt '     > Time '                     # Course start time
+  $freq = Read-Host -Prompt '     > Frequency '               # Course frequency
+
+  # Create shortcut on Desktop
+  $WshShell = New-Object -comObject WScript.Shell
+  $FilePath = "$Home\Desktop\" + $crs + ".url"
+  $Shortcut = $WshShell.CreateShortcut($FilePath)
+  $Shortcut.TargetPath = $url
+  $Shortcut.Save()
+
+  # Create a new task (docs.microsoft.com/en-us/powershell/module/scheduledtasks/new-scheduledtasktrigger)
+  # Task name : same as the course name
+  $taskName = $crs        
+  # Task action : Execute the created shortcut 
+  $taskAction = New-ScheduledTaskAction -Execute $FilePath
+  # Task Trigger : Set the task to trigger every week depending on the date & time
+  $taskTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday Tuesday -At $clk
+
+  # Finally, register the task
+  Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $taskAction `
+    -Trigger $taskTrigger `
 
   Write-Host('+ ' + $crs + ' [' + $url + ' | ' + $clk + ' | ' + $freq + ']', [environment]::newline) -ForegroundColor "Green"
 
